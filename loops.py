4644ccc91c00e3d55ef8369f22f9103e47320a55
Hema Deepthi Karmoory <hema@Hemas-MacBook-Air.local>
2018-12-01 23:23:13 -0600

    while, for loops

--- loops.py
+++ loops.py
@@ -6,3 +6,55 @@ Created on Sat Dec  1 21:47:08 2018
 @author: hema
 """
 
+input_number = int(input("enter a number: "))
+type(input_number)
+if(input_number < 10):
+    print("input number is < 10")
+elif(input_number == 10):
+    print("number is 10")
+else:
+    print("input number is > 10")
+    
+    
+# while loop
+
+i=0
+while(i <= 10):
+    print(i)
+    i+=2
+    
+    
+for i in range(0,11):
+    if(i%2 == 0):
+        print(i)
+    else:
+        pass
+
+    
+for i in range(0,11):
+    if(i%2 == 0):
+        print(i)
+
+tuple1 = ('mon','tue','wed')
+for i in tuple1:
+    print(i)
+    
+tuple2 = ('jan','feb','mar')
+for i in tuple2:
+    print(i,end=',')
+    
+a = 10
+for i in a:
+    print(i)
+    
+ 
+alphabets = 'abcde'
+for i in alphabets:
+    print(i.upper() ,end=',')
+    
+    
+    
+    
+    
+    
+    
